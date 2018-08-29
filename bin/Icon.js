0560ad3e53e714a70d55504e1c85c923d749f2a7
nicholasxjy <yitaqi2011@gmail.com>
2018-08-29 18:00:55 +0800

    modify icon.js tpl

--- bin/Icon.js
+++ bin/Icon.js
@@ -2,22 +2,19 @@
 
 import React from "react";
 
-const defaultStyle = {
-  display: "inline-flex",
-  alignItems: "center",
-  justifyContent: "center",
-  color: "rgba(67, 90, 111, 0.698)",
-  width: "20px",
-  height: "20px",
-  position: "relative"
-};
-
 export default class Icon extends React.Component {
   render() {
-    const { width, height, color, ...rest } = this.props;
-    const style = { ...defaultStyle, width, height, color };
+    const { size, color, ...rest } = this.props;
     return (
-      <span style={style} {...rest}>
+      <span
+        style={{
+          width: size + "px",
+          height: size + "px",
+          color
+        }}
+        {...rest}
+        className="svg-icon"
+      >
         {this.props.children}
       </span>
     );
